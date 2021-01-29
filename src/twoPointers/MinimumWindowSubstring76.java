0c571736df93c67f1da972fd39f9997897d8531f
fsdg129 <yaozuw@gmail.com>
2021-01-28 22:46:50 -0600

    Update the module Two Pointers

--- src/twoPointers/MinimumWindowSubstring76.java
+++ src/twoPointers/MinimumWindowSubstring76.java
@@ -1,9 +1,63 @@
 package twoPointers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumWindowSubstring76 {
 
-	private String name = "125. Valid Palindrome";
-	private String url = "https://leetcode.com/problems/valid-palindrome/";
+	private String name = "76. Minimum Window Substring";
+	private String url = "https://leetcode.com/problems/minimum-window-substring/";
 	
-	
+    public String minWindow(String s, String t) {
+        
+    	Map<Character, Integer> searchCharacter = new HashMap<>();
+    	Map<Character, Integer> needCharacter = new HashMap<>();
+    	for(char c : t.toCharArray()) {   		
+    		searchCharacter.put(c, 0);
+    		needCharacter.merge(c, 1, Integer::sum);
+    	}
+    	
+    	int foundCharacters = 0;
+    	int left = 0;
+    	int right = -1;
+    	char newChar;
+    	int minLeft=0, minRight=s.length();
+    	
+    	while(true) {
+    		
+    		if(foundCharacters < searchCharacter.size()) {
+    			right++;
+    			if(right > s.length() - 1) {
+    				break;
+    			}
+    			newChar = s.charAt(right);
+    			if(searchCharacter.containsKey(newChar)) {
+    				if(searchCharacter.get(newChar).intValue() == needCharacter.get(newChar).intValue() - 1) {
+    					foundCharacters++;
+    				}
+    				searchCharacter.compute(newChar, (k, v) -> Integer.sum(v, 1));	
+    			}
+    		} else {
+    			if(right - left < minRight - minLeft) {
+    				minLeft = left;
+    				minRight = right;
+    			}
+    			left++;
+    			newChar = s.charAt(left-1);
+    			if(searchCharacter.containsKey(newChar)) {
+    				searchCharacter.compute(newChar, (k, v) -> Integer.sum(v, -1));
+    				if(searchCharacter.get(newChar).intValue() < needCharacter.get(newChar).intValue()) {
+    					foundCharacters--;
+    				}
+    			}
+    		}
+    	}
+    	
+    	if(minRight >= s.length()) {
+    		return "";
+    	} else {
+    		return s.substring(minLeft, minRight+1);
+    	}
+    	  	
+    }
 }
--- src/twoPointers/SubarraysWithKDifferentIntegers992.java
+++ src/twoPointers/SubarraysWithKDifferentIntegers992.java
@@ -1,12 +1,48 @@
 package twoPointers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraysWithKDifferentIntegers992 {
 
 	private String name = "992. Subarrays with K Different Integers";
 	private String url = "https://leetcode.com/problems/subarrays-with-k-different-integers/";
 	
     public int subarraysWithKDistinct(int[] A, int K) {
+    	
+        Map<Integer, Integer> numberCounts = new HashMap<>();
+        int goodSubarray = 0;
         
+        int left = 0;
+        int right = -1;
+        int newNumber;
+        
+        while(true) {
+        	
+        	if(numberCounts.size() < K) {
+        		right++;
+        		if(right > A.length - 1) {
+        			break;
+        		}
+        		newNumber = A[right];
+        		numberCounts.merge(newNumber, 1, Integer::sum);
+        	} else {
+        		goodSubarray++;
+        		left++;
+        		newNumber = A[left - 1];
+        		numberCounts.compute(newNumber, (k,v) -> {
+        			if(v.intValue() > 1) {
+        				return Integer.sum(v, -1);
+        			} else {
+        				return null;
+        			}
+        		});
+        		
+        	}
+        	
+        }
+        
+        return goodSubarray;
     	
     }
 	
